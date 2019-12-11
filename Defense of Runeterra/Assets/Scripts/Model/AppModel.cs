152738f5cb7fbe5abe954d2b94d2c6d7f441ba9b
Martin Přívozník <martin.privoznik@pslib.cz>
2019-12-11 23:57:58 +0100

    Explosion - game end

--- Defense of Runeterra/Assets/Scripts/Model/AppModel.cs
+++ Defense of Runeterra/Assets/Scripts/Model/AppModel.cs
@@ -8,19 +8,23 @@ public class AppModel : MonoBehaviour
     public float Actual_Level = 1;
 
     private Camera _turret;
+    private GameObject _explosion;
 
     // Start is called before the first frame update
     void Start()
     {
         _turret = Camera.main;
+        _explosion = GameObject.Find("Tower/Explosion/exp1");
+        _explosion.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (_turret.GetComponent<PlayerControlModel>().TurretHP <= 0)
+        if (_turret.GetComponent<PlayerControlModel>().TurretActualHP <= 0 && _explosion.activeSelf == false)
         {
-
+            _turret.GetComponent<HeroesGenerator>().SpawningPeriod = -1; //Disable spawning
+            _explosion.SetActive(true);
         }
     }
 }
