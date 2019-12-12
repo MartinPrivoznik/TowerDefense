6532cab11c7c0019696db6e3d07587d4c184b2f0
Martin Přívozník <martin.privoznik@pslib.cz>
2019-12-12 02:40:58 +0100

    Defeat logo

--- Defense of Runeterra/Assets/Scripts/Model/AppModel.cs
+++ Defense of Runeterra/Assets/Scripts/Model/AppModel.cs
@@ -6,25 +6,36 @@ public class AppModel : MonoBehaviour
 {
 
     public float Actual_Level = 1;
+    public GameObject Defeat;
+    public GameObject Explosion;
 
     private Camera _turret;
-    private GameObject _explosion;
+    private SpriteRenderer _renderer;
+    private Color _alphaColor;
 
     // Start is called before the first frame update
     void Start()
     {
         _turret = Camera.main;
-        _explosion = GameObject.Find("Tower/Explosion/exp1");
-        _explosion.SetActive(false);
+
+        _renderer = Defeat.GetComponent<SpriteRenderer>();
+
+        _alphaColor = _renderer.material.color;
+        _alphaColor.a = 0;
+
+        Defeat.SetActive(false);
+        Explosion.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (_turret.GetComponent<PlayerControlModel>().TurretActualHP <= 0 && _explosion.activeSelf == false)
+        if (_turret.GetComponent<PlayerControlModel>().TurretActualHP <= 0 && Explosion.activeSelf == false)
         {
             _turret.GetComponent<HeroesGenerator>().SpawningPeriod = -1; //Disable spawning
-            _explosion.SetActive(true);
+
+            Explosion.SetActive(true);
+            Defeat.SetActive(true);
         }
     }
 }
